aeea4fbe840234a732c27142e9cae1a5b7e50ece
GENiEBEN <v@codin.uk>
2022-07-17 00:46:13 +0100

    Added 1507410

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 use std::io;
-mod game_1431240;
-mod game_914010;
+mod game_1431240; //Coloring Game for Adults
+mod game_1507410; //Coloring Game for Kids
+mod game_914010; //Train Station Renovation
 
 fn main() {
     loop {
@@ -11,6 +12,7 @@ fn main() {
             .expect("Failed to read input");
             match Some(&*cli_command.to_string().trim()) {
                 Some("check-progress 1431240") => println!("Checking progress in `Coloring Book for Adults`: {:?}", game_1431240::check_progress()),
+                Some("check-progress 1507410") => println!("Checking progress in `Coloring Book for Kids`: {:?}", game_1507410::check_progress()),
                 Some("test") => println!("{:?}", game_914010::get_save_dir()),
                 Some("quit") => std::process::exit(exitcode::OK),
                 _ => println!("Unknown command. Please see instructions on GitHub."),                                             
